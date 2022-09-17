26d7beb0093f65a7f8caaa7318244d90a3cbf56d
hamdysaad20 <hamdysaad.biz@gmail.com>
2022-09-18 00:15:51 +0200

    map issue

---
+++ json/History.js
@@ -0,0 +1,31 @@
+const History = [{
+    id:1,
+    image:"../public/Images/nasa.gif",
+    title:" test title text text-table sdds",
+    description:"test Attemptedimporterror:'$1'isnotexportedfrom"
+}
+,
+{
+    id:2,
+    image:"../public/Images/nasa.gif",
+    title:" test title text text-table sdds",
+    description:"test Attemptedimporterror:'$1'isnotexportedfrom"
+
+},
+{
+    id:3,
+    image:"../public/Images/nasa.gif",
+    title:" test title text text-table sdds",
+    description:"test Attemptedimporterror:'$1'isnotexportedfrom"
+
+},
+{
+    id:4,
+    image:"../public/Images/nasa.gif",
+    title:" test title text text-table sdds",
+    description:"test Attemptedimporterror:'$1'isnotexportedfrom"
+
+}
+
+]
+export default History
