67292cb1b63ee447e89433336dd36c0040cf751c
hamdysaad20 <hamdysaad.biz@gmail.com>
2022-09-18 01:46:15 +0200

    HistoryList

--- components/HistoryList.jsx
+++ components/HistoryList.jsx
@@ -10,15 +10,23 @@ import History from "../json/History"
   return (
     <div className="h-[300px]">
 <div className=" h-full">
-    <div className="gap-2 flex-wrap  mt-7 min-h-[200px] max-w-7xl justify-center flex mx-auto"> 
+    <div className="gap-2 flex-wrap px-1 mt-7 min-h-[200px] max-w-7xl justify-center flex mx-auto"> 
  
-    {History.sort(() => 0.5 - Math.random()).map((item, i) => (
-        <div key={i} className="bg-gray-700/20 backdrop-blur-md cursor-pointer hover:scale-105 shadow-lg duration-500  mt-2 w-[400px] rounded-md h-[160px]">
-        <Image src={ "/"+item.image} alt={item.title} height={10} width={10}/>
-        <h1>{item.title}</h1>
-        <p>{item.description}</p>
-        
-        
+    {History.sort(() => 0.5 - Math.random()).slice(0,6).map((item, i) => (
+        <div key={i} className="overflow-hidden bg-gray-700/20  gap-1 text-gray-400 flex backdrop-blur-md cursor-pointer hover:scale-[1.02] shadow-lg duration-500  mt-2 w-[400px] rounded-md h-[160px]">
+<div className="h-[200px] relative  w-[200px] ">
+<Image          
+        blurDataURL='data:...'
+   placeholder='blur'
+ src={ "/"+item.image} alt={item.title} 
+ height={200}
+width={200}
+/>
+</div>
+<div className="w-full pr-4  ">
+        <h1 className="text-xl text-gray-200 font-bold pt-4">{item.title}</h1>
+        <p className="pt-2  break-words ">{item.description.length >30?item.description.substring(0,20)+"..." :null }</p>
+        </div>   
         </div>
   
    
--- json/History.js
+++ json/History.js
@@ -1,9 +1,21 @@
  const History = [{
-    image:"../public/Images/nasa.gif",
+    image:"../public/Images/404.png",
     title:" test title text text-table sdds",
     description:"test Attemptedimporterror:'$1'isnotexportedfrom"
 }
 ,
+{
+    image:"../public/Images/nasa.gif",
+    title:" test title text text-table sdds",
+    description:"test Attemptedimporterror:'$1'isnotexportedfrom"
+
+},
+{
+    image:"../public/Images/nasa.gif",
+    title:" test title text text-table sdds",
+    description:"test Attemptedimporterror:'$1'isnotexportedfrom"
+
+},
 {
     image:"../public/Images/nasa.gif",
     title:" test title text text-table sdds",
