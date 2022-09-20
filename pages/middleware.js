8dc2ca478837e29a2917a72bc256911bdee1627f
hamdysaad20 <hamdysaad.biz@gmail.com>
2022-09-20 17:52:59 +0200

    remove the middelware

--- pages/middleware.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export function middleware() {   
-  const url = request.nextUrl.clone()   
-  if (url.pathname.includes("Search%3F=")) {
-    url.pathname = url.pathname.replace("Search%3F","Search?")
-    return NextResponse.redirect(url)   
-  } 
-}
