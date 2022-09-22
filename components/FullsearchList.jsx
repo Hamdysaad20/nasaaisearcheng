22958840551043afed758efc98a00fb377199a9e
hamdysaad20 <hamdysaad.biz@gmail.com>
2022-09-22 22:56:27 +0200

    tt

--- components/FullsearchList.jsx
+++ components/FullsearchList.jsx
@@ -55,7 +55,7 @@ function FullsearchList(props) {
       
 
 
-      <div onClick={()=>{setShow(false)}} className="absolute top-[120px] md:top-[140px] right-12 z-50">
+      <div onClick={()=>{setShow(false)}} className="absolute top-[124px] md:top-[140px] right-5 md:right-12 z-50">
 
       <svg xmlns="http://www.w3.org/2000/svg" width="24" fill="#e5e7eb" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>
 
